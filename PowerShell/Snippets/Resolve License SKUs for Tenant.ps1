1b3ef49677aabd247b95b8a8aa668de929573786
ITJoeSchmo <159281150+ITJoeSchmo@users.noreply.github.com>
2025-01-06 13:58:36 -0500

    Update Resolve License SKUs for Tenant.ps1
    backend API was changed for resolving License SKU -> DisplayName. New API endpoint requires sending along the SKUs you want to resolve.

--- PowerShell/Snippets/Resolve License SKUs for Tenant.ps1
+++ PowerShell/Snippets/Resolve License SKUs for Tenant.ps1
@@ -4,22 +4,23 @@
 # Incredibly useful when tying together license data from Graph and such. 
 
 Connect-AzAccount
+Connect-MgGraph 
 
 #74658136-14ec-4630-ad9b-26e160ff0fc6 is the AppId to https://main.iam.ad.ext.azure.com/api/AccountSkus?backfillTenants=false
-$Token = (Get-AzAccessToken -ResourceUrl "74658136-14ec-4630-ad9b-26e160ff0fc6").Token | ConvertTo-SecureString -AsPlainText -Force
-   
-$webData = Invoke-WebRequest -UseBasicParsing -Uri "https://main.iam.ad.ext.azure.com/api/AccountSkus?backfillTenants=false" `
-    -Headers @{
-        "Authorization"="Bearer $([PSCredential]::new("token", $($script:Token)).GetNetworkCredential().Password)"
-    }
+$Token = (Get-AzAccessToken -ResourceUrl "74658136-14ec-4630-ad9b-26e160ff0fc6" -AsSecureString).Token
 
-#convert JSON to objects
-$licenseData = $webData.Content | ConvertFrom-Json 
+$skuData        = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/subscribedSkus" -Method GET 
+[array]$allSKUs = $skuData.Value.skuId + $skuData.Value.servicePlans.servicePlanId | Sort -unique
 
-#make definition hashtable
-$resolveLicense = @{}
-
-#populate SKU:name definitions
-$licenseData | ForEach-Object { 
-  $resolveLicense.Add($_.skuId, $_.name) 
+$Body = @{
+      values = $allSKUs
+} | ConvertTo-Json
+$webData = Invoke-WebRequest -UseBasicParsing -Uri "https://main.iam.ad.ext.azure.com/api/Licenses/CatalogMetadata" -Method POST -ContentType "application/json" -Body $Body -Headers @{
+      "Authorization"="Bearer $([PSCredential]::new("token", $($script:Token)).GetNetworkCredential().Password)"
 }
+
+$licenseData = $webData.Content | ConvertFrom-Json # convert JSON to objects
+$licenseData = $licensedata.PSObject.Properties.Value # license data is actually nested under property where property name = sku ID, this un-tangles that
+# populate SKU:name definitions
+$resolveLicenseID = @{}
+$licenseData | ForEach-Object { $resolveLicenseID[$_.id] = $_.displayName }
